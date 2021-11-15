e95dae8181c2320751ad9bf5a141ed0a28d2ef9a
Jessica Aweya <jessaweya@gmail.com>
2021-11-15 04:12:30 +0000

    updating eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,44 @@
+const assertEqual = function (actual, expected) {
+
+    if (actual === expected) {
+        console.log(`Assertion Passed:${actual} ===${expected}`);
+        return true;
+    }
+    else {
+        console.log(`Assertion Failed:${actual} !== ${expected}`);
+        return false;
+    }
+};
+assertEqual(10, 10);
+assertEqual(5, 10);
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+        return false;
+    }
+}
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
+
+nction eqArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
