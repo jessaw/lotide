b0dea320e90876ca0ae1287b256651c8c25e725f
Jessica Aweya <jessaweya@gmail.com>
2021-11-15 03:45:01 +0000

    update findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+
+    if (actual === expected) {
+        console.log(`Assertion Passed:${actual} ===${expected}`);
+        return true;
+    }
+    else {
+        console.log(`Assertion Failed:${actual} !== ${expected}`);
+        return false;
+    }
+};
+assertEqual(10, 10);
+assertEqual(5, 10);
+
+const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama: "The Wire"
+};
+
+const findKeyByValue = function (object, value) {
+    for (let prop in object) {
+        if (object[prop] === value) {
+            return prop;
+        }
+    }
+}
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
