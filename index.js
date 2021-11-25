0396470904acfa3f11e78dc3421a1c1ceba46718
Jessica Aweya <jessaweya@gmail.com>
2021-11-25 02:59:36 +0000

    created index.js file

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const head = require('./head');
+const middle = require('./middle');
+const without = require('./without');
+const findKey = requre('./findKey');
+const takeUntil = require('./takeUntil');
+const countOnly = requre('./countOnly');
+
+
+module.exports = {
+    head: head,
+    middle: middle,
+    without: without,
+    findKey: findKey,
+    takeUntil: takeUntil,
+    countOnly: countOnly
+
+};
--- without.js
+++ without.js
@@ -38,3 +38,4 @@ assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
 
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => ["1", "2"]
 
+module.exports = without;
