d3d8093ff608675cb5666645119eb242a2ff18da
Jessica Aweya <jessaweya@gmail.com>
2021-11-26 02:56:47 +0000

    Fixing eqObject.js syntax and folder

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertEqual = function (actual, expected) {
 
     if (actual === expected) {
@@ -9,38 +11,31 @@ const assertEqual = function (actual, expected) {
         return false;
     }
 };
-assertEqual(10, 10);
-assertEqual(5, 10);
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+
+
 const eqObjects = function (object1, object2) {
-    if (Object.keys(object1).length !== Object.keys(object2).length) {
-        return false;
-    }
+    var obkey1 = Object.keys(object1).sort();
+    var obkey2 = Object.keys(object2).sort();
+    return JSON.stringify(obkey1) === JSON.stringify(obkey2);
 }
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
 
-function eqArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-    else {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) {
-                return false;
-            }
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
 
-        }
-        return true;
-    }
-}
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+
 
 module.exports = eqObjects;
--- middle.js
+++ middle.js
@@ -41,13 +41,11 @@ const middle = function (array) {
 
 };
 
-
-
 middle([1]) // => []
 middle([1, 2]) // => []
 middle([1, 2, 3]) // => [2]
 middle([1, 2, 3, 4, 5]) // => [3]
 
 console.log(middle([1, 2]));
-
+console.log(middle([1, 2, 3, 4, 5]));
 module.exports = middle;
